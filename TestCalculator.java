500d0dc951568258217a3ad8e79224558f915734
mahima303 <mmahalakshmi1@student.tce.edu>
2026-02-06 22:18:10 +0530

    Add main method to TestCalculator for testing

--- TestCalculator.java
+++ TestCalculator.java
@@ -1,3 +1,14 @@
 public class TestCalculator {
-    
+
+    public static void main(String[] args) {
+
+        Calculator c = new Calculator();
+
+        System.out.println("Addition: " + c.add(10,5));
+        System.out.println("Subtraction: " + c.sub(10,5));
+        System.out.println("Multiplication: " + c.mul(10,5));
+        System.out.println("Division: " + c.div(10,5));
+
+        System.out.println("All tests executed successfully");
+    }
 }
